693e671d7fc52f18ac8f3124f5b5f7d6d0c8ea83
amineaupec <amine.abdoul-azid@etu.u-pec.fr>
2023-04-27 19:42:09 +0200

    yacht yatch

--- infrasecufrei/TP1-minion/scripts/yatch-docker.sh
+++ infrasecufrei/TP1-minion/scripts/yatch-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo docker volume create yacht
-sudo docker run -d -p 8000:8000 --name yatch -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
+sudo docker run -d -p 8000:8000 --name yacht -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
+echo "http://$(hostname):8000"
+echo "admin@yacht.local"
+echo "pass"
