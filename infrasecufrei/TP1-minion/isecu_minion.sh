6719f5e3e7c7973f6821049b17218088b2d5c7fe
amineaupec <amine.abdoul-azid@etu.u-pec.fr>
2023-04-28 13:18:19 +0200

    isecu minion

---
+++ infrasecufrei/TP1-minion/isecu_minion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y fish zsh fzf docker.io docker-compose git avahi-daemon 
+docker pull portainer/portainer:latest
+docker volume create portainer_data
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer portainer/portainer:latest
+echo "portainer_url: http://$(hostname):9000"
