b23382c8b7175e34503a2cf87558ab7ecf19a00f
amineaupec <amine.abdoul-azid@etu.u-pec.fr>
2023-04-28 13:27:12 +0200

    minion

--- infrasecufrei/TP1-minion/isecu_minion.sh
+++ infrasecufrei/TP1-minion/isecu_minion.sh
@@ -5,3 +5,9 @@ docker pull portainer/portainer:latest
 docker volume create portainer_data
 docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer portainer/portainer:latest
 echo "portainer_url: http://$(hostname):9000"
+
+
+apt install awesome -y 
+apt install xinit -y
+echo "startx"
+apt install firefox -y
